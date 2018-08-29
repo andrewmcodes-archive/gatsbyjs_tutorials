9ba02882ab279012c091938e1c6f488751ca074a
andrewmcodes <andrew.mason@n2pub.com>
2018-08-29 19:46:51 -0400

    finish gatsbyjs tutorial

--- tutorial-part-four/gatsby-config.js
+++ tutorial-part-four/gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    siteMetadata: {
-    title: `Blah Blah Fake Title`,
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
